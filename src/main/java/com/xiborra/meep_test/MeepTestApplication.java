a98646d49f6d0eee2804d2a00b9e2fc5f553d8d9
xiborra <61407026+xiborra@users.noreply.github.com>
2021-05-13 15:13:29 +0200

    Init project

---
+++ src/main/java/com/xiborra/meep_test/MeepTestApplication.java
@@ -0,0 +1,13 @@
+package com.xiborra.meep_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeepTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeepTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xiborra/meep_test/MeepTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiborra.meep_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeepTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
